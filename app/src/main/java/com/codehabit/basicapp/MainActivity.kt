9a767b2f589b31b7720610ad8352833f5205424f
nasif-jaoadun <nasif_online@yahoo.com>
2023-07-18 21:59:23 +0600

    Example of an implicit intent

--- app/src/main/java/com/codehabit/basicapp/MainActivity.kt
+++ app/src/main/java/com/codehabit/basicapp/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.codehabit.basicapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +14,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        if(intent.action == Intent.ACTION_SEND){
+            val monster = intent?.getStringExtra(Intent.EXTRA_TEXT)
+            Log.i("Intent","You Selected monster $monster")
+            val monsterId = resources.getIdentifier(monster?.trim(),"drawable", packageName)
+            imageView2.setImageResource(monsterId)
+            return
+        }
         Log.i("Intents","Action =${intent.action}")
         val lastPart = intent.data?.lastPathSegment
         if(lastPart!!.equals("view")){
